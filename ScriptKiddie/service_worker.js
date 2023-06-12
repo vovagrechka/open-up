2fc84d917a8adc516a419b2420f370a85df62cb4
Volodymyr Grechka <volodymyr.grechka@globallogic.com>
2023-06-12 16:21:17 +0300

    wow

--- ScriptKiddie/service_worker.js
+++ ScriptKiddie/service_worker.js
@@ -19,13 +19,49 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
              * @param info {chrome.contextMenus.OnClickData}
              */
             func: function(info) {
-                // console.log("=== info", info)
-                const el = document.querySelector("img[src='" + info.srcUrl + "']")
-                if (!el) {
-                    alert("I don't understand this page, sorry")
-                    return
+                console.log("=== info", info)
+                try {
+                    const elImg = document.querySelector("img[src='" + info.srcUrl + "']")
+                    if (!elImg)
+                        throw Error("Image element not found by srcUrl")
+
+                    const mediaName = elImg.parentElement.getAttribute("data-test-media-name")
+                    if (!mediaName)
+                        throw Error("Can't extract media name from image parent")
+                    
+                    let url = ""
+                    
+                    for (const el of document.querySelectorAll("div[data-test-media-name='" + mediaName + "']")) {
+                        const p = el.parentElement?.parentElement
+                        if (p) {
+                            const d = p.getAttribute("data-testid")
+                            if (d) {
+                                const m = d.match(/\.attachment-id\.(\d+)$/)
+                                if (m) {
+                                    const attachmentId = m[1]
+                                    url = "https://bydeluxe.atlassian.net/secure/attachment/" + attachmentId + "/" + mediaName
+                                    break
+                                }
+                            }
+                        }
+                    }
+                    
+                    if (!url)
+                        throw Error("Can't guess attachment ID")
+
+                    navigator.clipboard.writeText(url)
+
+                    const elToast = document.createElement("div")
+                    elToast.setAttribute("style", "position: fixed; left: 10px; top: 10px; right: 10px; font-family: sans-serif; " +
+                        "font-size: 24px; z-index: 2147483647; background: #0d0; border: 3px solid black; padding: 5px; font-weight: bold;")
+                    elToast.innerText = "Copied URL to clipboard"
+                    document.body.append(elToast)
+                    window.setTimeout(() => elToast.remove(), 1500)
                 }
-                alert("Wow")
+                catch (e) {
+                    console.error(e)
+                    alert("[ERROR] " + e.message)
+                }
             }
         })
     }
